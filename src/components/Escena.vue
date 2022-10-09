1086d2b7e411abf8cd3206b954cbd9fada0348f4
IvanSanczewski <isanczewski@gmail.com>
2022-10-09 15:36:52 +0300

    Add project files. Exercise1 - Escena.vue - add template and prop // Home.vue - add component Escena and use it in template // App.vue - add component Home and use it in template

---
+++ src/components/Escena.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Our hero was surfing the Space when he spotted a spaceship</p>
+</template>
+
+<script>
+import Home from './Home.vue'
+
+export default {
+    props: ['sentence'],
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <Escena />
+</template>
+
+<script>
+import App from '../App.vue'
+import Escena from './Escena.vue'
+
+export default {
+    components: { Escena }
+}
+</script>
+
+<style>
+
+</style>
