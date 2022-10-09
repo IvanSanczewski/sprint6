c10988539e7c1c5a9ee14477aa0f41f3caf4203f
IvanSanczewski <isanczewski@gmail.com>
2022-10-09 22:20:52 +0300

    Ex2 -- Home.vue - add story arary with 4 objects cotiaining sentences and ids as key : value and binding them in template // Escena.vue - add prop and use it in v-for cile to display the value of sentence from story array

--- src/components/Escena.vue
+++ src/components/Escena.vue
@@ -1,19 +1,40 @@
 <template>
-    <p>{{ sentence }}</p>
+
+    <ul>
+        <li v-for="item in story" :key="item" class="">
+            {{item.sentence}}
+        </li>
+        <!-- <h4> {{ story.sentence }}</h4> -->
+    </ul>
+    <!-- <p>{{ sentenceStart }}</p> -->
+
 </template>
 
 <script>
-
+// import Home from './Home.vue'
 
 export default {
-    data(){
+    // component: { Home },
+    data(){ 
         return {
-            sentence: 'Our hero was surfing the Space when suddenly… he spotted a spaceship!'
+            sentenceStart: 'Our hero was surfing the Space when suddenly… he spotted a spaceship!',
+            //  story: [
+            //     {sentence: 'Our hero was surfing the Space when suddenly… he spotted a spaceship!', id: 1},
+            //     {sentence: 'He felt curious about the spaceship, so he entered in. Once there, he got to a room with two doors.', id: 2},
+            //     {sentence: 'The hero decided to take the door what would take him home.', id: 3},
+            //     {sentence: 'But the other heroes were not so lucky whith their choice.', id: 4 }
+            // ]
         }
-    }
+    },
+    props: ['story']
+
 }
 </script>   
 
 <style>
+ul {
+    list-style: none;
+    text-align: start;
+}
 
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,22 @@
 <template>
-    <Escena />
+    <Escena :prueba="prueba" :story="story"/>
 </template>
 
 <script>
 import Escena from './Escena.vue'
 
 export default {
-    components: { Escena }
+    components: { Escena },
+    data () {
+        return {
+            story: [
+                {sentence: 'Our hero was surfing the Space when suddenly… he spotted a spaceship!', id: 1},
+                {sentence: 'He felt curious about the spaceship, so he entered in. Once there, he got to a room with two doors.', id: 2},
+                {sentence: 'The hero decided to take the door what would take him home.', id: 3},
+                {sentence: 'But the other heroes were not so lucky whith their choice.', id: 4 }
+            ]
+        }
+    }
 
 }
 </script>
